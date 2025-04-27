2bbbc161154cb26a5c89e87c37e43b7fe5ce0f42
Sergio Uriona Herbas <seruriher@gmail.com>
2025-04-26 21:03:26 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import PWABadge from "./PWABadge.tsx";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">stock-values-dashboard</h1>
+      <PWABadge />
+    </>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,42 @@
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      strategies: "injectManifest",
+      srcDir: "src",
+      filename: "sw.ts",
+      registerType: "autoUpdate",
+      injectRegister: false,
+
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
+
+      manifest: {
+        name: "stock-values-dashboard",
+        short_name: "stock-values-dashboard",
+        description: "stock-values-dashboard",
+        theme_color: "#ffffff",
+      },
+
+      injectManifest: {
+        globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+      },
+
+      devOptions: {
+        enabled: true,
+        navigateFallback: "index.html",
+        suppressWarnings: true,
+        type: "module",
+      },
+    }),
+  ],
+});
